957a0714756593dc7db460989146d2cec6d168ef
cheater01 <1511867011@qq.com>
2016-09-10 10:21:13 +0800

    initial commit

---
+++ js/animate.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Cheater01 on 2016/9/9.
+ */
+function animate(obj,json,fn,time)
+{
+    clearInterval(obj.timer);
+    obj.timer=setInterval(function(){
+        var flag=true;
+        for(var attr in json)
+        {
+            var current=0;
+            if(attr=="opacity")
+            {
+                current=parseInt(getStyle(obj,attr)*100);
+            }
+            else
+            {
+                current=parseInt(getStyle(obj,attr));
+            }
+            var step=(json[attr]-current)/10;
+            step=step>0?Math.ceil(step):Math.floor(step);
+            if(attr=="opacity")
+            {
+                if("opacity" in obj.style)
+                {
+                    obj.style.opacity=(current+step)/100;
+                }
+                else
+                {
+                    obj.style.filter="alpha(opacity="+(current+step)+")";
+                }
+            }
+            else if(attr=="zIndex")
+            {
+                obj.style.zIndex=json[attr];
+            }
+            else
+            {
+                obj.style[attr]=current+step+"px";
+            }
+            if(current!=json[attr])
+            {
+                flag=false;
+            }
+        }
+        if(flag)
+        {
+            clearInterval(obj.timer);
+            if(fn)
+            {
+                fn();
+            }
+        }
+    },time);
+}
+
+function getStyle(obj,attr)
+{
+    if(obj.currentStyle)
+    {
+        return obj.currentStyle[attr];
+    }
+    else
+    {
+        return window.getComputedStyle(obj,null)[attr];
+    }
+}
+
